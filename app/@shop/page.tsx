81b71844ace24de7c5a4371aebcf879349989cd5
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-11-15 14:28:11 +0000

    Changed api fetch to Shop Layout

--- app/@shop/page.tsx
+++ app/@shop/page.tsx
@@ -1,15 +1,55 @@
-import ProductList from "../../components/ProductList";
+import Stripe from "stripe";
+import ProductList from "@/components/ProductList";
 
-export default function Shop({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <div>
-      <p>Shop</p>
-      {children}
-      <ProductList/>
-    </div>
-  );
-}
+type Product = Stripe.Product & {
+  default_price: Stripe.Price;
+};
+
+export const revalidate = 60;
+
+const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
+  apiVersion: "2024-10-28.acacia",
+});
+
+let products: Product[] = [];
+
+  try {
+    const response = await stripe.products.list({
+      limit: 10,
+      expand: ["data.default_price"],
+    });
+
+    if (response.data.length === 0) {
+      throw new Error("No products found");
+    }
+
+    const activeProducts = response.data.filter((product) => {
+      return product.active;
+    });
+    const finalProducts = activeProducts.filter((product) => {
+      return Number(product.metadata.stock) > 0
+    })
+    //Add dynamic filter option passed as props using URL search PArams to pass between layers and filter based on metadata.
+  
+
+    products = finalProducts.map((product) => ({
+      ...product,
+      default_price: product.default_price as Stripe.Price,
+    }));
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+
+  console.log(products);
+
+export default function Shop() {
+
+
+    return (
+      <div>
+        <p>Shop</p>
+        <ProductList products={products} />
+        
+      </div>
+    );
+  }
--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -2,46 +2,12 @@ import Card from "./Card";
 import Stripe from "stripe";
 
 type Product = Stripe.Product & {
-  default_price: Stripe.Price;
-};
+    default_price: Stripe.Price;
+  };
 
-export const revalidate = 60;
 
-const stripe = new Stripe(process.env.STRIPE_SECRET ?? "", {
-  apiVersion: "2024-10-28.acacia",
-});
-
-const ProductList = async () => {
-  let products: Product[] = [];
-
-  try {
-    const response = await stripe.products.list({
-      limit: 10,
-      expand: ["data.default_price"],
-    });
-
-    if (response.data.length === 0) {
-      throw new Error("No products found");
-    }
-
-    const activeProducts = response.data.filter((product) => {
-      return product.active;
-    });
-    const finalProducts = activeProducts.filter((product) => {
-      return Number(product.metadata.stock) > 0
-    })
-    //Add dynamic filter option passed as props using URL search PArams to pass between layers and filter based on metadata.
+const ProductList = async ({ products } : { products : Product[] }) => {
   
-
-    products = finalProducts.map((product) => ({
-      ...product,
-      default_price: product.default_price as Stripe.Price,
-    }));
-  } catch (error) {
-    console.error("Error fetching products:", error);
-  }
-
-  console.log(products);
 
   return (
     <div className="bg-white">
