df6c2c53cba5b43584de4ccfcc7b9f7e8e7c77fa
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-11-16 13:09:13 +0000

    tidied css

--- components/NavBar2.tsx
+++ components/NavBar2.tsx
@@ -4,59 +4,35 @@ import Link from "next/link";
 export default function NavBar2() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         <Link
           href="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <Image
             src="/favicon.ico"
-            className="h-8"
             alt="Logo"
-            width="30"
-            height="30"
+            width={30}
+            height={30}
           />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="text-xl font-semibold whitespace-nowrap dark:text-white">
             LAMPMAN
           </span>
         </Link>
-        
-        <div className="hidden w-full md:block md:w-auto" id="navbarn">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                href="/shop"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
-              >
-                Shop
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Basket
-              </Link>
-            </li>
-          </ul>
+
+        <div className="hidden md:flex space-x-8 rtl:space-x-reverse">
+          <Link href="/shop" className="text-blue-700 hover:text-blue-900 dark:text-blue-500 dark:hover:text-blue-700">
+            Shop
+          </Link>
+          <Link href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-400">
+            About
+          </Link>
+          <Link href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-400">
+            Contact
+          </Link>
+          <Link href="#" className="text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-400">
+            Basket
+          </Link>
         </div>
       </div>
     </nav>
