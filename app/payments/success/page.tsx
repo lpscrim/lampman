92433219153250f935314aa9b7cdfc97ed465f50
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-11-28 14:21:04 +0000

    updated colours to custom

--- app/payments/success/page.tsx
+++ app/payments/success/page.tsx
@@ -82,55 +82,55 @@ export default async function PaymentSuccess(props: {
 
   return (
     <div>
-      <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+      <section className="border-t-2 border-b-2 border-background bg-secondary py-8 antialiased md:py-16">
         <div className="mx-auto max-w-2xl px-4 2xl:px-0">
-          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl mb-2">
+          <h2 className="text-xl font-semibold text-text1 sm:text-2xl mb-2">
             Thanks for your order!
           </h2>
-          <p className="text-gray-500 dark:text-gray-400 mb-6 md:mb-8">
+          <p className="text-text1h mb-6 md:mb-8">
             Your order{" "} <span className="text-logo">{String(invoiceNum)}</span> {" "}
             
             will be processed within 24 hours during working days. We will
             notify you by email once your order has been shipped.
           </p>
-          <div className="space-y-4 sm:space-y-2 rounded-lg border border-gray-100 bg-gray-50 p-6 dark:border-gray-700 dark:bg-gray-800 mb-6 md:mb-8">
+          <div className="space-y-4 sm:space-y-2 rounded-lg border border-text3 p-6 bg-secondaryh mb-6 md:mb-8">
             <dl className="sm:flex items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h ">
                 Date of order
               </dt>
-              <dd className="font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium text-text1 sm:text-end">
                 {date}
               </dd>
             </dl>
             <dl className="sm:flex items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Customer ID
               </dt>
-              <dd className="font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium text-text1 sm:text-end">
                 {customerId}
               </dd>
             </dl>
             <dl className="sm:flex items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Payment ID
               </dt>
-              <dd className="font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium text-text1 sm:text-end">
                 {String(paymentId)}
               </dd>
             </dl>
             <dl className="sm:flex pt-8 items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Customer Name
               </dt>
-              <dd className="capitalize font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="capitalize font-medium text-text1 sm:text-end">
                 {invoiceName}
               </dd>
             </dl>
             <dl className="sm:flex items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Address
               </dt>
-              <dd className="font-medium py-4 text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium py-4 text-text1 sm:text-end">
                 {address?.line1}<br></br>
                 {address?.line2}<br></br>
                 {address?.city}<br></br>
@@ -139,18 +139,18 @@ export default async function PaymentSuccess(props: {
               </dd>
             </dl>
             <dl className="sm:flex items-center justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Email
               </dt>
-              <dd className="font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium text-text1 sm:text-end">
                 {String(email)}
               </dd>
             </dl>
             <dl className="sm:flex items-center py-8 justify-between gap-4">
-              <dt className="font-normal mb-1 sm:mb-0 text-gray-500 dark:text-gray-400">
+              <dt className="font-normal mb-1 sm:mb-0 text-text1h">
                 Total
               </dt>
-              <dd className="font-medium text-gray-900 dark:text-white sm:text-end">
+              <dd className="font-medium text-text1 sm:text-end">
                 £{amount ? Math.round(amount/ 100 ).toFixed(2) : ''}
               </dd>
             </dl>
@@ -158,9 +158,9 @@ export default async function PaymentSuccess(props: {
           <div className="flex items-center space-x-4">
             <Link
               href="/"
-              className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              className="py-2.5 px-5 text-sm font-bold text-secondary focus:outline-none bg-primary rounded-lg border focus:z-10 focus:ring-4 focus:ring-text1  border-text2 hover:primaryh"
             >
-              Return to shopping
+              Return To Shop
             </Link>
           </div>
         </div>
