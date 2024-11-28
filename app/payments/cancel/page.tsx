0f147e9eae789294e679862f492253eb9d8e736e
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-11-28 20:34:26 +0000

    created actual payment cancel page

--- app/payments/cancel/page.tsx
+++ app/payments/cancel/page.tsx
@@ -1,14 +1,18 @@
-
-
+import Link
+ from "next/link";
 export default function PaymentFail() {
-
-    return (
-        <div>
-            <p>
-                fail 
-            </p>
-        </div>
-    )
+  return (
+    <div className="text-center h-screen py-20 px-20">
+      <h2 className="text-4xl pt-44 font-semibold mb-4">Something went wrong!</h2>
+      <p className="text-xl font-semibold mb-10">Payment was cancelled</p>
+      <div className="w-full items-center space-x-4">
+              <Link
+                href="/"
+                className="py-2.5 px-5 text-sm font-bold text-secondary focus:outline-none bg-primary rounded-lg border focus:z-10 focus:ring-4 focus:ring-text1  border-text2 hover:primaryh"
+              >
+                Return To Shop
+              </Link>
+            </div>
+    </div>
+  );
 }
-//check for query params if none this might break the stock method
-//release stock back using meta +1
