4dfcbe30ec19b5551e814ea42d3f10257b28fb45
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-11-29 15:52:27 +0000

    replaced priority for hero

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             width={500}
             height={500}
             placeholder="blur"
+            priority
           />
           <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
