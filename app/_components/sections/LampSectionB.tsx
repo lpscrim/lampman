8023b1b55efad8f6b214e8370ba2216e98966753
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-08 16:26:25 +0000

    added to new lap section b

--- app/_components/sections/LampSectionB.tsx
+++ app/_components/sections/LampSectionB.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
+import lamp from "../../../public/blue2.5.jpg";
 
 export default function LampSection() {
   return (
     <div className="mt-12 pt-6 max-w-screen-xl   relative rounded-md z-1 flex flex-col top-8 mx-auto w-4/5 bg-secondary opacity-85 justify-center items-center h-auto text-center">
       <div className="container mx-auto px-4 h-full">
-        <h1 className="text-4xl font-bold mb-8 text-center text-text1">
-          The Significance of 1950s Vintage Lamps
-        </h1>
-
+        <h1 className="text-4xl font-bold text-center">Mac Lamp</h1>
+        <h2 className="text-2xl mt-2 mb-8">by Terence Conran</h2>
         <div className="grid grid-cols-1 md:grid-cols-2  gap-8 mb-16">
-          <div className="relative overflow-hidden h-80 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+          <div className="max-w-lg relative overflow-hidden h-96">
             <Image
-              src="https://www.londonfine.co.uk/cdn/shop/products/18.7310-1_1024x1024.jpg?v=1614602289"
-              alt="1950s vintage lamp"
+              src={lamp}
+              alt="vintage lamp"
               className="object-cover"
               fill
             />
-            <div className="absolute inset-0 bg-primary opacity-20"></div>
             <div className="text-primary absolute bottom-0 left-0 right-0 flex justify-between p-4 bg-text3 opacity-90">
-              <h2 className="text-xl font-bold">Iconic Design</h2>
+              <h2 className="text-xl font-bold">Mac Lamp</h2>
               <button className="bg-primary text-secondary font-semibold px-4 py-2 rounded hover:bg-primaryh">
                 Learn More
               </button>
@@ -28,20 +26,40 @@ export default function LampSection() {
           <div>
             <h2 className="text-2xl font-semibold mb-4">Why They Matter</h2>
             <ul className="list-disc pl-5 space-y-2">
-              <li>Represent mid-century modern aesthetic</li>
-              <li>Symbolize nostalgia and retro appeal</li>
-              <li>Influence contemporary lighting design</li>
-              <li>Appreciated for their craftsmanship and durability</li>
+              <li>
+                The Mac Lamps represent a shift towards modernist ideals in
+                post-war Britain, emphasizing practicality and aesthetic
+                simplicity.
+              </li>
+              <li>
+                With their innovative use of materials and adjustable forms,
+                these lamps were a breakthrough in blending art with utility.
+              </li>
+              <li>
+                Terence Conran&#39s Mac Lamps remain collector&#39s items,
+                cherished for their historical value and timeless appeal in
+                design.
+              </li>
             </ul>
           </div>
         </div>
 
-        <p className="text-xl mb-8">
-          1950s vintage lamps continue to captivate interior designers and
-          homeowners alike due to their unique blend of functionality,
-          aesthetics, and historical significance. These lamps represent a
-          pivotal moment in design history, bridging the gap between traditional
-          craftsmanship and innovative materials.
+        <p className="text-xl mb-8 text-pretty">
+          Terence Conran, a renowned British designer and entrepreneur, is
+          celebrated for his contributions to modern design, particularly in
+          furniture and lighting. His Mac Lamps are a quintessential example of
+          mid-20th-century industrial design, blending functionality with sleek,
+          minimalist aesthetics.
+          <br /><br />
+          Designed in the 1950s, these lamps showcase Conran&#39s commitment to
+          democratizing good design by making it both accessible and stylish.
+          The Mac Lamps were inspired by Scandinavian design principles,
+          featuring clean lines, practical adjustability, and efficient use of
+          materials. <br /><br />
+          Their understated elegance, combined with high functionality, made
+          them iconic in contemporary interiors. They embodied Conran&#39s
+          philosophy that everyday objects should enhance life through
+          thoughtful design.
         </p>
       </div>
     </div>
