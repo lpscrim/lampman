56fbfb8881ed6ccd45b47c5862ab67f942b8265c
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-09 13:03:21 +0000

    added referrer policy?

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ]
+      }
+    ]
+  },
   async redirects() {
     return [
       {
