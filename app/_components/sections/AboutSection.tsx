24277dfcd126e5758cc7483df8268225ae5b446c
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-10 12:47:13 +0000

    updated about message

--- app/_components/sections/AboutSection.tsx
+++ app/_components/sections/AboutSection.tsx
@@ -9,16 +9,14 @@ export default function AboutSection() {
       <h1 className="text-3xl font-bold leading-tight mt-4 mb-2">About</h1>
       <div className="flex flex-row">
         <p className="text-md h-5/6 mt-2 mb-4 px-6 lg:text-base text-pretty">
-          At [LAMPMAN], we&#39;re passionate about breathing new life into the
-          past.<br></br>Specializing in the restoration and refurbishment of
-          antique lamps, we transform forgotten treasures into stunning,
-          functional pieces for modern homes. <br></br>
-          Beyond lighting, our collection boasts a fascinating array of oddities
-          and curios—unique finds that spark curiosity and tell their own
-          stories. <br></br>
+          Specializing in the restoration and refurbishment of
+          antique lamps and antiques, we transform forgotten classic items into stunning,
+          functional pieces for modern homes.<br></br>
+          Each item is refurbished with care, ensuring that the original functionality is 
+          restored whilst the iconic look is tastefully preserved.<br></br>
           Whether you&#39;re looking for a one-of-a-kind statement piece or a
-          touch of vintage charm, we&#39;re here to help you bring character and
-          wonder into your space. <br></br>
+          touch of vintage charm, our selection of lamps and curios will bring 
+          authentic character and style into your home. <br></br>
         </p>
       </div>
       <div className="mx-4 relative overflow-hidden h-96 rounded-sm">
