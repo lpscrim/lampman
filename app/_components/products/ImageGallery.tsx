8e5110ca17deb8654fb3aa63ff5ada99406d29d9
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-12 23:08:11 +0000

    Added pointer cursors

--- app/_components/products/ImageGallery.tsx
+++ app/_components/products/ImageGallery.tsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 import Stripe from "stripe";
 import ImageViewer from "@/app/_components/products/ImageViewer";
 
-
 type Product = Stripe.Product;
 
 export default function ImageGallery({ product }: { product: Product }) {
@@ -42,90 +41,90 @@ export default function ImageGallery({ product }: { product: Product }) {
 
   const handleOffClicked = () => {
     setSelectedImage(null);
-  }
-
+  };
 
   return (
     <>
-      
       {selectedImage && (
-        <ImageViewer 
+        <ImageViewer
           selectedImage={selectedImage}
           onNext={handleNextImage}
           onPrev={handlePreviousImage}
           offClick={handleOffClicked}
         />
       )}
-      
-      <div className="xl:w-2/5 lg:w-2/5 w-80 md:block hidden">
+
+      <div className="lg:w-2/5 w-80 md:block hidden">
         {product.images.length > 0 ? (
-
-            <Image
-              alt={`Product ${product.id}`}
-              src={product.images[0]}
-              className="rounded-md object-center"
-              width={512}
-              height={342}
-              priority
-              onClick={() => handleOnClicked(product.images[0] as string, 0)}
-            />
-
-        ) : null }
+          <Image
+            alt={`Product ${product.id}`}
+            src={product.images[0]}
+            className="cursor-pointer rounded-md object-center "
+            width={512}
+            height={342}
+            priority
+            onClick={() => handleOnClicked(product.images[0] as string, 0)}
+          />
+        ) : null}
         {product.metadata.img1 ? (
-            <Image
-              alt={`Product ${product.metadata.img1}`}
-              src={product.metadata.img1}
-              className="rounded-md object-center "
-              width={512}
-              height={682.5}
-              priority
-              onClick={() => handleOnClicked(product.metadata.img1 as string, 1)}
-            />
+          <Image
+            alt={`Product ${product.metadata.img1}`}
+            src={product.metadata.img1}
+            className="cursor-pointer rounded-md object-center"
+            width={512}
+            height={682.5}
+            priority
+            onClick={() => handleOnClicked(product.metadata.img1 as string, 1)}
+          />
         ) : null}
       </div>
       <div className="md:hidden">
         {product.images.length > 0 ? (
-            <Image
-              alt={`Product ${product.id}`}
-              src={product.images[0]}
-              className="w-full h-auto object-cover rounded-md"
-              width={800}
-              height={800}
-              onClick={() => handleOnClicked(product.images[0] as string, 0)}
-            />
+          <Image
+            alt={`Product ${product.id}`}
+            src={product.images[0]}
+            className="cursor-pointer w-full h-auto object-cover rounded-md"
+            width={800}
+            height={800}
+            onClick={() => handleOnClicked(product.images[0] as string, 0)}
+          />
         ) : null}
         <div className="flex flex-row items-center justify-between mt-3 space-x-4 md:space-x-0 overflow-hidden ">
           {product.metadata.img1 ? (
-      
-              <Image
-                alt={`Product ${product.id}`}
-                src={product.metadata.img1}
-                width={200}
-                height={200}
-                className="rounded-md"
-                onClick={() => handleOnClicked(product.metadata.img1 as string, 1)}
-              />
+            <Image
+              alt={`Product ${product.id}`}
+              src={product.metadata.img1}
+              width={200}
+              height={200}
+              className="cursor-pointer rounded-md"
+              onClick={() =>
+                handleOnClicked(product.metadata.img1 as string, 1)
+              }
+            />
           ) : null}
           {product.metadata.img2 ? (
-              <Image
-                alt={`Product ${product.id}`}
-                src={product.metadata.img2}
-                className="rounded-md"
-                width={200}
-                height={200}
-                onClick={() => handleOnClicked(product.metadata.img1 as string, 2)}
-              />
+            <Image
+              alt={`Product ${product.id}`}
+              src={product.metadata.img2}
+              className="cursor-pointer rounded-md"
+              width={200}
+              height={200}
+              onClick={() =>
+                handleOnClicked(product.metadata.img1 as string, 2)
+              }
+            />
           ) : null}
           {product.metadata.img3 ? (
-
-              <Image
-                alt={`Product ${product.id}`}
-                src={product.metadata.img3}
-                className="rounded-md"
-                width={200}
-                height={200}
-                onClick={() => handleOnClicked(product.metadata.img1 as string, 3)}
-              />
+            <Image
+              alt={`Product ${product.id}`}
+              src={product.metadata.img3}
+              className="cursor-pointer rounded-md"
+              width={200}
+              height={200}
+              onClick={() =>
+                handleOnClicked(product.metadata.img1 as string, 3)
+              }
+            />
           ) : null}
         </div>
       </div>
