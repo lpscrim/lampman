f0dc0fe48f5f63e3c3b91c86c69a3bf7662da8da
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-16 21:16:48 +0000

    further adjustments to button

--- app/_components/navigation/NavBar2.tsx
+++ app/_components/navigation/NavBar2.tsx
@@ -22,7 +22,7 @@ export default function NavBar2() {
   return (
     <>
       <nav className="z-[9999] bg-secondary">
-        <div className="text-[1.05rem] container mx-auto px-4 py-3 flex justify-between items-center">
+        <div className=" container mx-auto px-4 py-3 flex justify-between items-center">
           <Link
             href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
