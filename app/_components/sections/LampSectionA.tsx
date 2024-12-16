0b3d5a80df39c13f8d5924b803645c9ecc847539
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-16 20:27:23 +0000

    remoed button for link for SEO

--- app/_components/sections/LampSectionA.tsx
+++ app/_components/sections/LampSectionA.tsx
@@ -35,10 +35,8 @@ export default function LampSection() {
               fill
             />
             <div className="text-primary absolute bottom-0 left-0 right-0 flex justify-between p-4 bg-text3 opacity-90">
-              <Link target="_target" href="https://www.houseandgarden.co.uk/article/anglepoise-lamp">
-                <button className="bg-primary text-secondary font-semibold px-4 py-2 rounded hover:bg-primaryh">
-                  Learn More
-                </button>
+              <Link target="_target" href="https://www.houseandgarden.co.uk/article/anglepoise-lamp" className="bg-primary text-secondary font-semibold px-4 py-2 rounded hover:bg-primaryh">
+                Learn More
               </Link>
             </div>
           </div>
