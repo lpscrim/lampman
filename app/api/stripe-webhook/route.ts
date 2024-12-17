90a6490f074efc440432f8d8e9b88ecdbaffa11e
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2024-12-17 23:18:31 +0000

    made sure secret is as string

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -40,7 +40,7 @@ export async function POST(request: NextRequest) {
     event = stripe.webhooks.constructEvent(
       body,
       sig,
-      process.env.STRIPE_WEBHOOK_SECRET ?? ""
+      process.env.STRIPE_WEBHOOK_SECRET as string
     );
   } catch (err: unknown) {
     if (err instanceof Error) {
